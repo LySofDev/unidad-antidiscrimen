3d0bec0680ce9ffb73d01083e1adab790eb7431a
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-03-05 14:24:59 -0400

    Removed commented instructions

--- test/system/client_deliveries_test.rb
+++ test/system/client_deliveries_test.rb
@@ -1,13 +1,10 @@
 require "application_system_test_case"
 
 class ClientDeliveriesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit client_deliveries_url
-  #
-  #   assert_selector "h1", text: "ClientDelivery"
-  # end
+
   test "visit the React client" do
     visit client_delivery_url
     assert_selector "div", text: "Hello React!"
   end
+  
 end
