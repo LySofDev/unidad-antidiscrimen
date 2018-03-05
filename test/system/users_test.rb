dd408f65e36e6363bd36661f497e962dcd0ce8d1
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-03-05 15:50:49 -0400

    Added user registration and sign in wit test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -3,14 +3,26 @@ require "application_system_test_case"
 class UsersTest < ApplicationSystemTestCase
 
   test "User can register in" do
+    user = build(:user)
     visit new_user_registration_url
-    fill_in 'Fist Name', with: 'Esteban'
-    fill_in 'Last Name', with: 'Hernandez'
-    fill_in 'Email', with: 'esteban.hernandez@gmail.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password Confirmation', with: 'password'
+    fill_in 'First Name', with: user.first_name
+    fill_in 'Last Name', with: user.last_name
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password Confirmation', with: user.password_confirmation
     click_on 'Sign Up'
-    assert_select 'div.flash.flash-success', "Registration completed!"
+    assert_not page.has_content? 'errors prohibited this user from being saved'
+    assert page.has_content? 'Welcome! You have signed up successfully.'
+  end
+
+  test "User can sign in" do
+    user = create(:user)
+    visit new_user_session_url
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Sign In'
+    assert_not page.has_content? 'errors prohibited this user from being saved'
+    assert page.has_content? 'Signed in successfully.'
   end
 
 end
