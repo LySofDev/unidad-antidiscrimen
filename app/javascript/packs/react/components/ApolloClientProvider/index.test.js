f5763775095fffef827817587c390006296286d2
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-03-05 17:07:29 -0400

    Extracted Enzyme configuration

--- app/javascript/packs/react/components/ApolloClientProvider/index.test.js
+++ app/javascript/packs/react/components/ApolloClientProvider/index.test.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
+import Enzyme from '../Enzyme'
 import ApolloClientProvider, { getApolloClient } from './'
 
 const TestComponent = getApolloClient(({ apollo }) => apollo ? (<div>Success</div>) : (<div>Failure</div>))
-
-Enzyme.configure({ adapter: new Adapter() })
 
 test("It mounts successfully", () => {
   const wrapper = Enzyme.mount(<ApolloClientProvider />)
