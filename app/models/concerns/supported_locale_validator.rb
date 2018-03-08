268ca6ecd9b15fb5f126b98cb79bcacbc427fccd
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-03-08 10:55:37 -0400

    Improved structure

--- app/models/concerns/supported_locale_validator.rb
+++ app/models/concerns/supported_locale_validator.rb
@@ -1,9 +1,15 @@
 class SupportedLocaleValidator < ActiveModel::EachValidator
+
   def validate_each(record, attribute, value)
-    supported_locales_path = Rails.root.join("lib", "assets", "locales.yml")
-    supported_locales = YAML.load(open(supported_locales_path))
     unless supported_locales.include? value
       record.errors[attribute] << (options[:message] || "is not supported.")
     end
   end
+
+  private
+
+  def supported_locales
+    YAML.load(open(Rails.root.join("lib", "assets", "locales.yml")))
+  end
+  
 end
